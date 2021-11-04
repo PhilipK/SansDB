9efabfe972d3babac0c418c9aff332280abd23c8
Philip Kristoffersen <philipkristoffersen@gmail.com>
2021-11-04 21:20:48 +0100

    Initial scaffold

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use serde::{Deserialize, Serialize, de::DeserializeOwned};
+use std::collections::VecDeque;
+use std::sync::{Arc, RwLock};
+
+
 fn main() {
+
     println!("Hello, world!");
 }
+
+
+struct SansDB<Command,Data> where
+Command : Sync + Serialize + DeserializeOwned,
+Data: Sync + Serialize +  DeserializeOwned {
+    data: Arc<RwLock<Data>>,
+    que: VecDeque<Command>
+}
+
