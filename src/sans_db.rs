337b78ed3e4c18cfce1f0003381ab1cdb964f50c
Philip Kristoffersen <philipkristoffersen@gmail.com>
2021-11-06 13:45:56 +0100

    Send a stop command when db is dropped

--- src/sans_db.rs
+++ src/sans_db.rs
@@ -13,19 +13,23 @@ pub struct SansDB<Command,Data> where
 Command : Sync + Send + Clone + Serialize  ,
 Data: Sync + Send + Serialize + DeserializeOwned {
     data: Arc<RwLock<Data>>,
-    sender: SyncSender<Command>,
+    sender: SyncSender<InnerCommand<Command>>,
     write_handle : Option<thread::JoinHandle<()>>,
-    finish_up: Arc<RwLock<bool>>
 }
 
 impl <Command,Data> Drop for SansDB<Command,Data> where
 Command : Sync + Send + Clone + Serialize  ,
 Data: Sync + Send + Serialize + DeserializeOwned {
     fn drop(&mut self) {
-        *self.finish_up.write().unwrap() = true;
+        self.sender.send(InnerCommand::Stop).unwrap();
         self.write_handle.take().unwrap().join().unwrap();
-
     }
+}
+
+
+enum InnerCommand<Command> {
+    Stop,
+    Actual(Command)
 }
 
 
@@ -54,60 +58,59 @@ where Command : 'static + Sync + Send + Clone + Serialize + DeserializeOwned,
 
           pub fn write(&self, command:Command) { 
               //remove unwrap
+              let command = InnerCommand::Actual(command);
               self.sender.send(command).unwrap();
           }
 
 
           pub fn new(data:Data, storage_location:&Path, command_handler: fn(&Command, &mut Data) -> ())  -> Self{
               // 0 means, lock on writes untill a handler is ready
-              let (sender,receiver) = sync_channel::<Command>(1000);
+              let (sender,receiver) = sync_channel::<InnerCommand<Command>>(1000);
               let data = Arc::new(RwLock::new(data)); 
               let inner_data = data.clone();
 
-              let finish_up = Arc::new(RwLock::new(false)); 
-              let finish_up_inner = finish_up.clone();
               let storage_path_buff = storage_location.to_path_buf();
               let write_handle = thread::spawn(move || {
                   loop {
-                      if *finish_up_inner.read().unwrap(){
-                          return;
-                      }
-                      let command = receiver.recv();
+                     let command = receiver.recv();
                       if command.is_err(){
                           // this happens if all senders are gone
                           return;
                       }
-                      
-                      let command = command.unwrap();
-                      let serialize_command = command.clone();
+                      match command.unwrap(){
+                        InnerCommand::Stop => return,
+                        InnerCommand::Actual(command) => {
+                          let serialize_command = command.clone();
 
-                      let storage_path_buff = storage_path_buff.to_path_buf();
-                      let ser_handle = thread::spawn(move || {
-                          let data: Vec<u8> = bincode::serialize(&serialize_command).unwrap();
-                          let now = SystemTime::now();
-                          use std::time:: UNIX_EPOCH;
-                          let since_the_epoch = now
-                              .duration_since(UNIX_EPOCH)
-                              .expect("Time went backwards");
-                          let file_name = format!("{:?}", since_the_epoch.as_nanos());
-                          use std::io::prelude::*;
-                          let mut file = File::create(storage_path_buff.join(file_name)).unwrap();
-                          //get rid of this unwrap
-                          file.write_all(&data).unwrap();
-                      });
-                      {
-                          let write_lock = inner_data.write();
-                          let mut write_guard = write_lock.unwrap();
-                          (command_handler)(&command,&mut write_guard);
-                      }
-                      ser_handle.join().unwrap();
+                          let storage_path_buff = storage_path_buff.to_path_buf();
+                          let ser_handle = thread::spawn(move || {
+                              let data: Vec<u8> = bincode::serialize(&serialize_command).unwrap();
+                              let now = SystemTime::now();
+                              use std::time:: UNIX_EPOCH;
+                              let since_the_epoch = now
+                                  .duration_since(UNIX_EPOCH)
+                                  .expect("Time went backwards");
+                              let file_name = format!("{:?}", since_the_epoch.as_nanos());
+                              use std::io::prelude::*;
+                              let mut file = File::create(storage_path_buff.join(file_name)).unwrap();
+                              //get rid of this unwrap
+                              file.write_all(&data).unwrap();
+                          });
+                          {
+                              let write_lock = inner_data.write();
+                              let mut write_guard = write_lock.unwrap();
+                              (command_handler)(&command,&mut write_guard);
+                          }
+                          ser_handle.join().unwrap();
+
+                      },
+                    }
                   }
               });
               Self{
                   data,
                   sender,
-                  write_handle:Some(write_handle),
-                  finish_up
+                  write_handle:Some(write_handle)
               }
 
           }
--- todo.md
+++ todo.md
@@ -1,15 +1,3 @@
 # Todo
-- Commands to change state
-- command serializable
-- make it generic
-- rwlock for read/write acces
-- must have a handler of C (generic over command)
-- handler gets a mutable reference to state
-- command que
-- thread for writing commands to disk
-- restore from disk
 - snapshot restore
 - create snapshot
-
-## Questions
-Should we create two states... let people read while others are writing and then stop giving access to old?
